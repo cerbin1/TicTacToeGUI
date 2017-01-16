1346af030cf7354cfc35154ebda83fead9fe2416
Bartek <bartek9393@poczta.onet.pl>
2017-01-16 19:08:49 +0100

    Moved code to Application

--- src/Application/Application.java
+++ src/Application/Application.java
@@ -1,8 +1,44 @@
 package Application;
 
+import javax.swing.*;
 import java.awt.*;
 
 public class Application {
+    private char previousChar = 'x';
+    private JButton[] buttons;
+
+    Application(String title) {
+        createGameFrame(title);
+    }
+
+    void createGameFrame(String title) {
+        JFrame frame = new JFrame(title);
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setLayout(new GridLayout(3, 3));
+        buttons = new JButton[9];
+        for (int i = 0; i < 9; i++) {
+            JButton button = new JButton();
+            button.addActionListener(event -> createActionListener(button));
+            buttons[i] = button;
+            frame.add(button);
+        }
+        frame.setVisible(true);
+    }
+
+    private void createActionListener(JButton button) {
+        if (button.getText().equals("")) {
+            button.setIcon(new ImageIcon("src\\images\\" + previousChar + ".jpg"));
+            if (previousChar == 'o') {
+                previousChar = 'x';
+            } else {
+                previousChar = 'o';
+            }
+        } else {
+            System.out.println("Pole zajete");
+        }
+    }
+
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> new Window("Tic Tac Toe"));
     }
